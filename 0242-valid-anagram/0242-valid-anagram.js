6f3fba5ca8ec05c2db32a9317d8906f2c99b5a25
김성호 <120389368+Banana-Master@users.noreply.github.com>
2023-04-10 10:43:15 +0900

    Time: 79 ms (72.79%), Space: 48.6 MB (15.57%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -4,20 +4,7 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    let answer = true;
-    s = s.split('');
-    t = t.split('');
-    for (let i = 0; i < s.length; i++) {
-        let j = t.indexOf(s[i]);
-        if (j === -1) {
-            answer = false;
-            break;
-        } else {
-            t.splice(j, 1);
-        }
-    }
-    if (t.length !== 0) {
-        answer = false;
-    }
-    return answer;
+    s = s.split('').sort()
+    t = t.split('').sort()
+    return s.join('') == t.join('');
 };
